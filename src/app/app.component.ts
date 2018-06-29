e4f076950d1374a2223b846e0769c02163abec12
Gilad <gilad@gilads-mbp.mynet>
2018-06-29 10:50:23 +0300

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SocketService} from './services/socket-service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+    public list = [];
+    public form;
+    public user: any = {};
+
+
+    constructor(public socketService: SocketService) {
+        this.user.index = this.getRandomInt();
+
+        this.socketService.initSocket();
+
+        this.socketService.onMessage().subscribe((message) => {
+
+            if (message.user.index === this.user.index && message.action === 'CHECK') {
+                return;
+            }
+
+            if (message.user.index === this.user.index && message.action === 'DELETE') {
+                const exists = this.list.filter((p) => (p.index === message.product.index));
+                if (exists.length) {
+                    const index = this.list.indexOf(exists[0]);
+                    this.list.splice(index, 1);
+                }
+                return;
+            }
+
+
+            this.list = message.list;
+        });
+    }
+
+    getRandomInt(min = 0, max = 1000000) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            item: new FormControl('')
+        });
+    }
+
+    delete(product) {
+        this.socketService.send({
+            action: 'DELETE',
+            user: this.user,
+            product: product
+        });
+    }
+
+    add(productName) {
+
+        const product = {
+            index: Math.random(),
+            name: productName,
+            checked: false,
+            removed: false,
+            user: this.user
+        };
+
+        this.socketService.send({
+            action: 'ADD',
+            user: this.user,
+            product: product
+        });
+
+        this.form.reset();
+    }
+
+    onChecked(event, product) {
+        product.checked = event.checked;
+        this.socketService.send(Object.assign({}, {
+            action: 'CHECK',
+            user: this.user,
+            product: product
+        }));
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MaterialModule} from './shared/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MAT_CHECKBOX_CLICK_ACTION} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        {provide: MAT_CHECKBOX_CLICK_ACTION, useValue: 'check'}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
