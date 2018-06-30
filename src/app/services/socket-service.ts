4e5e8ff35de0edf95e9c5c5040bd7367feeb91e9
Gilad <gilad@gilads-mbp.mynet>
2018-07-01 02:15:28 +0300

    build

--- src/app/services/socket-service.ts
+++ src/app/services/socket-service.ts
@@ -1,22 +1,21 @@
 import {Injectable, isDevMode} from '@angular/core';
 
-import { Message } from '../shared/model/message';
-import { Event } from '../shared/model/event';
+import {Message} from '../shared/model/message';
+import {Event} from '../shared/model/event';
 
 import * as socketIo from 'socket.io-client';
 import {Observable} from 'rxjs';
 
-const SERVER_URL = 'http://localhost:8080';
-const BASE_URL = isDevMode() ? 'http://localhost:8080' : 'http://104.197.52.12/shopoing_api';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SocketService {
     private socket;
+    private baseUrl = isDevMode() ? 'http://localhost:8080' : 'http://104.197.52.12/shopoing_api';
 
     public initSocket(): void {
-        this.socket = socketIo(SERVER_URL);
+        this.socket = socketIo(this.baseUrl);
     }
 
     public send(message: any): void {
